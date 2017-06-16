87d756bb1b1b84d6950c639a334163b6d4ead58b
Alberto Kato <albertokatojr@gmail.com>
2017-06-16 16:35:04 -0300

    game logic I

--- memoryGame.js
+++ memoryGame.js
@@ -14,8 +14,32 @@ function pickCard(card)
 	var backCard  = $(card).parent();
 	var frontCard = backCard.next();
 
+	frontCard.addClass('active');
+	var numActiveCards = $('.active');
+
 	cardHide(backCard);
 	cardShow(frontCard);
+
+	if (numActiveCards.length == 2) {
+		numActiveCards.eq(0).removeClass('active');
+		numActiveCards.eq(1).removeClass('active');
+		
+		if (numActiveCards.eq(0).attr('name') === numActiveCards.eq(1).attr('name')) {
+			alert('Its a match');
+		}
+		else {
+			//back
+			//front
+			backCard1 = numActiveCards.eq(0).prev();
+			backCard2 = numActiveCards.eq(1).prev();
+			cardHide(numActiveCards.eq(0));
+			cardHide(numActiveCards.eq(1));
+			cardShow(backCard1);
+			cardShow(backCard2);
+			
+			alert('you fail');
+		}
+	}
 }
 
 function cardHide(card)
@@ -54,11 +78,11 @@ function buildGrid(numberOfCards,frontCardsImgs,backCardImg)
 	for (var i = 0 ; i < numberOfCards ; i++) {
   		frontCardImg = frontCardsImgs[i];
   		grid += '<div class="col-lg-2 col-sm-3 col-xs-4">' 
-  						+'<div>'+
+  						+'<div class="backCard">'+
              				'<img src="'+ backCardImg +'" class="thumbnail" onclick="pickCard(this);"> '
         				+'</div>' 
   				
-  			        	+'<div class="frontCard" >'+
+  			        	+'<div class="frontCard" name="'+ frontCardImg  +'" >'+
              				'<img src="'+ frontCardImg +'"  class="thumbnail">'
         				+'</div>' 	
    	 			+'</div>';
